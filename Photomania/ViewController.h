ee101ca0113fe08cb14e47270d86ac3a72ba4e05
Kyle Rogers <kyle.rogers@gmail.com>
2013-10-02 11:40:56 -0500

    Initial Commit

---
+++ Photomania/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Photomania
+//
+//  Created by Kyle Rogers on 10/2/13.
+//  Copyright (c) 2013 Kyle Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Photomania/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photomania
+//
+//  Created by Kyle Rogers on 10/2/13.
+//  Copyright (c) 2013 Kyle Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
